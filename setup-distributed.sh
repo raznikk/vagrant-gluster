21ff670db99186a94de5df50289eb946dbc16c0c
Barry Britt [LAS] <bbritt@iastate.edu>
2016-11-10 22:43:59 -0600

    Updating scripts

---
+++ setup-distributed.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+#
+# Command line argument: name of the volume
+#
+
+vagrant ssh server1 -c "sudo gluster peer probe server2; sudo gluster peer probe server3"
+bin/dssh "sudo mkdir /gluster/brick{1..3}/brick 2&>1 >/dev/null"
+vagrant ssh server1 -c "sudo gluster volume create $1 server1:/gluster/brick1/brick server2:/gluster/brick1/brick server3:/gluster/brick1/brick"
+vagrant ssh server1 -c "sudo gluster volume start $1"
